327c50d58746efab9532ef74d6611b2b87dc5735
chanwoo <wkdcksdn1996@gmail.com>
2022-10-28 02:18:21 +0900

    typescript start

---
+++ 1-2-function.ts
@@ -0,0 +1,29 @@
+{
+  // JavaScript 💩
+  function jsAdd(num1, num2) {
+    return num1 + num2;
+  }
+
+  // TypeScript ✨
+  function tsAdd(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+
+  // JavaScript 💩
+  function jsFetchNum(id) {
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+
+  // TypeScript ✨
+  function tsFetchNum(id) {
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+}
